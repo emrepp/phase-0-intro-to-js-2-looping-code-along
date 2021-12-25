9cf840ae3a50d4aa741b98e89e18d4fd5e4344b3
Emma Repp <emmarepp.draws@gmail.com>
2021-12-24 15:51:22 -1000

    commit tree 40d4804b960dd417897b2eab2aea2268448e1a0c

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let countdown = 10;
-while (countdown >=0) {
-  console.log(countdown--);
+
+function writeCards (names, event) {
+  let messages = [];
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+
+function countDown(positiveNumber) {
+  while(positiveNumber >= 0) {
+    console.log(positiveNumber)
+    positiveNumber--;
+  }
+
 }
